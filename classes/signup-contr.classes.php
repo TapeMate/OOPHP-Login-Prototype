aaacce5f24b0ca5e05620da85a32ff4fe5231863
TapeMate <admin@tapemate.onmicrosoft.com>
2024-01-02 17:07:24 +0100

    integrated Regex check on Uid.

--- classes/signup-contr.classes.php
+++ classes/signup-contr.classes.php
@@ -25,4 +25,15 @@ class SignupContr
         }
         return $result;
     }
+
+    private function invalidUid()
+    {
+        $result = null;
+        if (!preg_match("/^[a-zA-Z0-9]*$/", $this->uid)) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
+    }
 }
