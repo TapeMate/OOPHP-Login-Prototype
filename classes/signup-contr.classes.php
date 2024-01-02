5aece05213a2e3c9554afaa25cbd3cdc0b84f92c
TapeMate <admin@tapemate.onmicrosoft.com>
2024-01-02 16:44:55 +0100

    setup SignupContr class with props and constructor.

---
+++ classes/signup-contr.classes.php
@@ -0,0 +1,18 @@
+<?php
+
+class SignupContr
+{
+
+    private $uid;
+    private $pwd;
+    private $pwdrepeat;
+    private $email;
+
+    public function __construct($uid, $pwd, $pwdrepeat, $email)
+    {
+        $this->uid = $uid;
+        $this->pwd = $pwd;
+        $this->pwdrepeat = $pwdrepeat;
+        $this->email = $email;
+    }
+}
